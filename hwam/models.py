3d998c4bf5a335ce2f463de5b1d68bae1c4cddc4
dogoncouch <dpersonsdev@gmail.com>
2018-01-12 17:51:57 -0500

    Update hwam models

--- hwam/models.py
+++ hwam/models.py
@@ -8,8 +8,10 @@ class HardwareAsset(models.Model):
     asset_name = models.CharField(max_length=30)
     asset_desc = models.CharField(max_length=200)
     date_added = models.DateTimeField('date added')
+    date_eol = models.DateTimeField('end of life')
 
 class SystemAsset(models.Model):
+    parent_hardware = models.ManyToManyField(HardwareAsset)
     asset_owner = models.CharField(max_length=30)
     asset_custodian = models.CharField(max_length=30)
     asset_name = models.CharField(max_length=30)
