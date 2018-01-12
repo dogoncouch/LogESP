96483053e7130e42d25a8801c8127c1bd2885a88
dogoncouch <dpersonsdev@gmail.com>
2018-01-12 18:03:44 -0500

    Add hwam to project

--- ldsi/settings.py
+++ ldsi/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'hwam.apps.HwamConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
