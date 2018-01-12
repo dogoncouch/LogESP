0ef40682997b0378a321ab42a2d1daa956effd5a
dogoncouch <dpersonsdev@gmail.com>
2018-01-12 17:21:43 -0500

    Add gitignore

--- ldsi/urls.py
+++ ldsi/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
+    path('hwam/', include('hwam.urls')),
     path('admin/', admin.site.urls),
 ]
