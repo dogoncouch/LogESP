1b1a6cd7828d559ac25f8f4c16c85b54408267ac
dogoncouch <dpersonsdev@gmail.com>
2018-01-12 17:42:42 -0500

    Add hwam database models

--- hwam/models.py
+++ hwam/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class HardwareAsset(models.Model):
+    asset_owner = models.CharField(max_length=30)
+    asset_custodian = models.CharField(max_length=30)
+    asset_name = models.CharField(max_length=30)
+    asset_desc = models.CharField(max_length=200)
+    date_added = models.DateTimeField('date added')
+
+class SystemAsset(models.Model):
+    asset_owner = models.CharField(max_length=30)
+    asset_custodian = models.CharField(max_length=30)
+    asset_name = models.CharField(max_length=30)
+    asset_desc = models.CharField(max_length=200)
+    date_added = models.DateTimeField('date added')
+
