2894aea152e6adafc2fbe5590fc1c51cadb1ca24
dogoncouch <dpersonsdev@gmail.com>
2018-01-18 18:54:32 -0500

    Require login for hwam creation

--- hwam/urls.py
+++ hwam/urls.py
@@ -1,5 +1,6 @@
 
 from django.urls import path
+from django.contrib.auth.decorators import login_required
 
 from . import views
 
@@ -12,8 +13,11 @@ urlpatterns = [
     path('ou/<int:pk>/', views.OUDetailView.as_view(), name='ou_detail'),
     path('hw/<int:pk>/', views.HWDetailView.as_view(), name='hw_detail'),
     path('sw/<int:pk>/', views.SWDetailView.as_view(), name='sw_detail'),
-    path('ou/add/', views.OUCreateView.as_view(), name='ou_create'),
-    path('hw/add/', views.HWCreateView.as_view(), name='hw_create'),
-    path('sw/add/', views.SWCreateView.as_view(), name='sw_create'),
+    path('ou/add/', login_required(views.OUCreateView.as_view()),
+        name='ou_create'),
+    path('hw/add/', login_required(views.HWCreateView.as_view()),
+        name='hw_create'),
+    path('sw/add/', login_required(views.SWCreateView.as_view()),
+        name='sw_create'),
 ]
 
