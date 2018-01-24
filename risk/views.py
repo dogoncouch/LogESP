6874df2b2eaa42137c68559441062822db50ee4c
dogoncouch <dpersonsdev@gmail.com>
2018-01-24 14:56:08 -0500

    Add standard permissions to risk

--- risk/views.py
+++ risk/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
 from django.views.generic import ListView, DetailView, DeleteView
 from django.views.generic.edit import CreateView, UpdateView
+from django.contrib.auth.mixins import PermissionRequiredMixin
 
 from .models import AdvThreatEvent, NonAdvThreatEvent
 from .models import AdvThreatSource, NonAdvThreatSource
@@ -88,152 +89,173 @@ class ImpactDetailView(DetailView):
     template_name = 'risk/impact_detail.html'
     context_object_name = 'impact'
 
-class ATECreateView(CreateView):
+class ATECreateView(PermissionRequiredMixin, CreateView):
     model = AdvThreatEvent
+    permission_required = 'risk.add_advthreatevent'
     fields = ['name', 'desc', 'event_type', 'sources', 'relevance',
             'info_source', 'tier', 'likelihood_initiation', 
             'likelihood_impact', 'vulnerabilities', 'impacts']
     def get_success_url(self):
         return reverse_lazy('risk:ate_detail', args=(self.object.id,))
 
-class NTECreateView(CreateView):
+class NTECreateView(PermissionRequiredMixin, CreateView):
     model = NonAdvThreatEvent
+    permission_required = 'risk.add_nonadvthreatevent'
     fields = ['name', 'desc', 'event_type', 'sources', 'relevance',
             'info_source', 'tier', 'likelihood_initiation', 
             'likelihood_impact', 'risk_conditions', 'impacts']
     def get_success_url(self):
         return reverse_lazy('risk:nte_detail', args=(self.object.id,))
 
-class ATSCreateView(CreateView):
+class ATSCreateView(PermissionRequiredMixin, CreateView):
     model = AdvThreatSource
+    permission_required = 'risk.add_advthreatsource'
     fields = ['name', 'desc', 'source_type', 'info_source', 'tier',
             'in_scope', 'capability', 'intent', 'targeting']
     def get_success_url(self):
         return reverse_lazy('risk:ats_detail', args=(self.object.id,))
 
-class NTSCreateView(CreateView):
+class NTSCreateView(PermissionRequiredMixin, CreateView):
     model = NonAdvThreatSource
+    permission_required = 'risk.add_nonadvthreatsource'
     fields = ['name', 'desc', 'source_type', 'info_source', 'tier',
             'in_scope', 'range_of_effect']
     def get_success_url(self):
         return reverse_lazy('risk:nts_detail', args=(self.object.id,))
 
-class VulnCreateView(CreateView):
+class VulnCreateView(PermissionRequiredMixin, CreateView):
     model = Vulnerability
+    permission_required = 'risk.add_vulnerability'
     fields = ['name', 'desc', 'vuln_type', 'severity',
             'info_source', 'tier']
     def get_success_url(self):
         return reverse_lazy('risk:vuln_detail', args=(self.object.id,))
 
-class CondCreateView(CreateView):
+class CondCreateView(PermissionRequiredMixin, CreateView):
     model = RiskCondition
+    permission_required = 'risk.add_riskcondition'
     fields = ['name', 'desc', 'condition_type', 'pervasiveness',
             'info_source', 'tier']
     def get_success_url(self):
         return reverse_lazy('risk:cond_detail', args=(self.object.id,))
 
-class ImpactCreateView(CreateView):
+class ImpactCreateView(PermissionRequiredMixin, CreateView):
     model = Impact
+    permission_required = 'risk.add_impact'
     fields = ['name', 'desc', 'impact_type', 'info_source', 'tier',
             'severity', 'impact_tier']
     def get_success_url(self):
         return reverse_lazy('risk:ate_detail', args=(self.object.id,))
 
-class ATEUpdateView(UpdateView):
+class ATEUpdateView(PermissionRequiredMixin, UpdateView):
     model = AdvThreatEvent
+    permission_required = 'change_advthreatevent'
     fields = ['name', 'desc', 'event_type', 'sources', 'relevance',
             'info_source', 'tier', 'likelihood_initiation', 
             'likelihood_impact', 'vulnerabilities', 'impacts']
     def get_success_url(self):
         return reverse_lazy('risk:ate_detail', args=(self.object.id,))
 
-class NTEUpdateView(UpdateView):
+class NTEUpdateView(PermissionRequiredMixin, UpdateView):
     model = NonAdvThreatEvent
+    permission_required = 'change_nonadvthreatevent'
     fields = ['name', 'desc', 'event_type', 'sources', 'relevance',
             'info_source', 'tier', 'likelihood_initiation', 
             'likelihood_impact']
     def get_success_url(self):
         return reverse_lazy('risk:nte_detail', args=(self.object.id,))
 
-class ATSUpdateView(UpdateView):
+class ATSUpdateView(PermissionRequiredMixin, UpdateView):
     model = AdvThreatSource
+    permission_required = 'change_advthreatsource'
     fields = ['name', 'desc', 'source_type', 'info_source', 'tier',
             'in_scope', 'capability', 'intent', 'targeting']
     def get_success_url(self):
         return reverse_lazy('risk:ats_detail', args=(self.object.id,))
 
-class NTSUpdateView(UpdateView):
+class NTSUpdateView(PermissionRequiredMixin, UpdateView):
     model = NonAdvThreatSource
+    permission_required = 'change_nonadvthreatsource'
     fields = ['name', 'desc', 'source_type', 'info_source', 'tier',
             'in_scope', 'range_of_effect']
     def get_success_url(self):
         return reverse_lazy('risk:nts_detail', args=(self.object.id,))
 
-class VulnUpdateView(UpdateView):
+class VulnUpdateView(PermissionRequiredMixin, UpdateView):
     model = Vulnerability
+    permission_required = 'change_vulnerability'
     fields = ['name', 'desc', 'vuln_type', 'severity',
             'info_source', 'tier']
     def get_success_url(self):
         return reverse_lazy('risk:vuln_detail', args=(self.object.id,))
 
-class CondUpdateView(UpdateView):
+class CondUpdateView(PermissionRequiredMixin, UpdateView):
     model = RiskCondition
+    permission_required = 'change_riskconditions'
     fields = ['name', 'desc', 'condition_type', 'pervasiveness',
             'info_source', 'tier']
     def get_success_url(self):
         return reverse_lazy('risk:cond_detail', args=(self.object.id,))
 
-class ImpactUpdateView(UpdateView):
+class ImpactUpdateView(PermissionRequiredMixin, UpdateView):
     model = Impact
+    permission_required = 'change_impact'
     fields = ['name', 'desc', 'impact_type', 'info_source', 'tier',
             'severity', 'impact_tier']
     def get_success_url(self):
         return reverse_lazy('risk:impact_detail', args=(self.object.id,))
 
-class ATEDeleteView(DeleteView):
+class ATEDeleteView(PermissionRequiredMixin, DeleteView):
     model = AdvThreatEvent
+    permission_required = 'delete_advthreatevent'
     template_name = 'risk/ate_delete.html'
     context_object_name = 'ate'
     def get_success_url(self):
         return reverse('risk:ate_index')
 
-class NTEDeleteView(DeleteView):
+class NTEDeleteView(PermissionRequiredMixin, DeleteView):
     model = NonAdvThreatEvent
+    permission_required = 'delete_nonadvthreatevent'
     template_name = 'risk/nte_delete.html'
     context_object_name = 'nte'
     def get_success_url(self):
         return reverse('risk:nte_index')
 
-class ATSDeleteView(DeleteView):
+class ATSDeleteView(PermissionRequiredMixin, DeleteView):
     model = AdvThreatSource
+    permission_required = 'delete_advthreatsource'
     template_name = 'risk/ats_delete.html'
     context_object_name = 'ats'
     def get_success_url(self):
         return reverse('risk:ats_index')
 
-class NTSDeleteView(DeleteView):
+class NTSDeleteView(PermissionRequiredMixin, DeleteView):
     model = NonAdvThreatSource
+    permission_required = 'delete_nonadvthreatsource'
     template_name = 'risk/nts_delete.html'
     context_object_name = 'nts'
     def get_success_url(self):
         return reverse('risk:nts_index')
 
-class VulnDeleteView(DeleteView):
+class VulnDeleteView(PermissionRequiredMixin, DeleteView):
     model = Vulnerability
+    permission_required = 'delete_vulnerability'
     template_name = 'risk/vuln_delete.html'
     context_object_name = 'vuln'
     def get_success_url(self):
         return reverse('risk:vuln_index')
 
-class CondDeleteView(DeleteView):
+class CondDeleteView(PermissionRequiredMixin, DeleteView):
     model = RiskCondition
+    permission_required = 'delete_riskcondition'
     template_name = 'risk/cond_delete.html'
     context_object_name = 'cond'
     def get_success_url(self):
         return reverse('risk:cond_index')
 
-class ImpactDeleteView(DeleteView):
+class ImpactDeleteView(PermissionRequiredMixin, DeleteView):
     model = Impact
+    permission_required = 'delete_impact'
     template_name = 'risk/impact_delete.html'
     context_object_name = 'impact'
     def get_success_url(self):
