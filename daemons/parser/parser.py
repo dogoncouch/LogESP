5d465678de8143e41f1cbef399847be1c8f98315
dogoncouch <dpersonsdev@gmail.com>
2018-02-05 19:31:49 -0500

    Update parse helper logic

--- daemons/parser/parser.py
+++ daemons/parser/parser.py
@@ -100,7 +100,7 @@ class ParseModule:
             for h in self.parsehelpers:
                 extmatch = re.findall(h['regex_format'], line)
                 if extmatch:
-                    extlist = list(zip(fields, extmatch[0]))
+                    extlist = list(zip(h['fields'], extmatch[0]))
                     for f, v in extlist:
                         entry[f] = v
 
