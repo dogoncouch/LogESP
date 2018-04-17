79002b7217b49d729ef3a5a1ffa1c312d6bd764d
dogoncouch <dpersonsdev@gmail.com>
2018-04-17 00:43:52 -0400

    Update limit rule model

--- siem/models.py
+++ siem/models.py
@@ -97,7 +97,7 @@ class LimitRule(models.Model):
     name = models.CharField(max_length=32)
     desc = models.CharField(max_length=200,
             null=True, blank=True)
-    is_builtin = models.BooleanField(default=False)
+    is_builtin = models.BooleanField(default=False, blank=True)
     is_enabled = models.BooleanField(default=True)
     reverse_logic = models.BooleanField(default=False)
     email_alerts = models.BooleanField(default=False)
